4c07ca5353026680e04dcf6fa23be0f0fef163cd
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-02-13 11:01:58 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -4,10 +4,10 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    	<title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+                <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
 	<meta name="description" content="<?= $Wcms->page('description') ?>">
 	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
