217e85933b6e17c3e7230cc24c5101a2a428f2d0
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-02-13 11:45:20 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -11,16 +11,17 @@
 	        <meta name="description" content="<?= $Wcms->page('description') ?>">
 	        <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
-	<meta property="og:url" content="<?= $this->url() ?>" />
-	<meta property="og:type" content="website" />
-	<meta property="og:site_name" content="<?= $Wcms->get('config', 'siteTitle') ?>" />
-	<meta property="og:title" content="<?= $Wcms->page('title') ?>" />
-	<meta name="twitter:site" content="<?= $this->url() ?>" />
-	<meta name="twitter:title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
-	<meta name="twitter:description" content="<?= $Wcms->page('description') ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="css/main.css" />
-		<noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
+	        <meta property="og:url" content="<?= $this->url() ?>" />
+	        <meta property="og:type" content="website" />
+	        <meta property="og:site_name" content="<?= $Wcms->get('config', 'siteTitle') ?>" />
+	        <meta property="og:title" content="<?= $Wcms->page('title') ?>" />
+                <meta name="twitter:site" content="<?= $this->url() ?>" />
+		<meta name="twitter:title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
+		<meta name="twitter:description" content="<?= $Wcms->page('description') ?>" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="<?= $Wcms->asset('css/main.css') ?>" />
+		<?= $Wcms->css() ?>
+		<noscript><link rel="stylesheet" href="<?= $Wcms->asset('css/noscript.css') ?>" /></noscript>
 	</head>
 	<body class="is-preload">
 		<!-- Wrapper -->
@@ -31,8 +32,8 @@
 						<div class="inner">
 
 							<!-- Logo -->
-								<a href="index.html" class="logo">
-									<span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">Phantom</span>
+								<a href="<?= $Wcms->url() ?>" class="logo">
+									<span class="symbol"><img src="images/logo.svg" alt="logo" /></span><span class="title"><?= $Wcms->get('config','siteTitle') ?></span>
 								</a>
 
 							<!-- Nav -->
@@ -117,6 +118,7 @@
 			<script src="js/breakpoints.min.js"></script>
 			<script src="js/util.js"></script>
 			<script src="js/main.js"></script>
-
+		        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+	                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>
